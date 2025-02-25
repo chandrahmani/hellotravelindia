9dd5f94d26f2cec51296bdcdb0759dc35c3e6e0f
Chand Rahmani <chandrahmani254@gmail.com>
2025-02-26 01:16:04 +0530

    added images gallery section tour and travel

--- src/components/common/HomePage.tsx
+++ src/components/common/HomePage.tsx
@@ -10,6 +10,7 @@ import {
 import ImageSlider from "../core/modal/ImageSlider";
 import travelPackages from "../../data/data.json";
 import { useEffect, useState } from "react";
+import { Gallery } from "./Gallery";
 
 type travelPackage = {
   title: string;
@@ -25,32 +26,33 @@ const HomePage = () => {
   return (
     <div>
       <ImageSlider />
-      <Container sx={{ mt: 5 }}>
-        <Typography variant="h4" align="center" gutterBottom>
-          Explore Our Exclusive Travel Packages
-        </Typography>
-        <Grid container spacing={5}>
-          {packages.map((pkg, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <Card sx={{ mb: 5 }}>
-                <CardMedia>
-                  <CardContent>
-                    <img src={pkg.image} width={"500"} height={"200"} />
-                    <Typography variant="h6">{pkg.title}</Typography>
-                    <Typography variant="h5" color="textSecondary">
-                      {pkg.description}
-                    </Typography>
 
-                    <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-                      Book Now
-                    </Button>
-                  </CardContent>
-                </CardMedia>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      <Typography variant="h4" align="center" gutterBottom>
+        Explore Our Exclusive Travel Packages
+      </Typography>
+      <Grid container spacing={5}>
+        {packages.map((pkg, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card sx={{ mb: 5 }}>
+              <CardMedia>
+                <CardContent>
+                  <img src={pkg.image} width={"500"} height={"200"} />
+                  <Typography variant="h6">{pkg.title}</Typography>
+                  <Typography variant="h5" color="textSecondary">
+                    {pkg.description}
+                  </Typography>
+
+                  <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+                    Book Now
+                  </Button>
+                </CardContent>
+              </CardMedia>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Gallery />
     </div>
   );
 };
