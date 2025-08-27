3290bc86743e5be8256d1081a6e293f6f77b3442
chandrahmani <chandrahmani254@gmail.com>
2025-08-28 03:35:41 +0530

    Update dependencies and refactor contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,14 @@
 import { Gallery } from "@/components/common/Gallery";
-import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, TextField } from "@mui/material";
 import { useState } from "react";
 
 export default function ContactPage() {
   const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const message = `Hello Travel India! My name is ${name},and  i am interested in your tour packages.${phone}.`;
+    const message = `Hello Travel India! My name is ${name},and  i'm interested in your tour packages.`;
     const encodedMessage = encodeURIComponent(message);
     const whatsappNumber = "7006036459";
 
@@ -31,6 +30,9 @@ export default function ContactPage() {
             gap: 2,
             maxWidth: 400,
             mx: "auto",
+            background: '#638892',
+            padding:3,
+            borderRadius: 2
           }}
         >
           <TextField
@@ -38,13 +40,6 @@ export default function ContactPage() {
             variant="outlined"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <TextField
-            label="Your Phone Number"
-            variant="outlined"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
             required
           />
           <Button type="submit" variant="contained" color="primary">
