9c1cf07dbc7bb1ecaf9d8329f3de0fc80f8a71a7
chandrahmani <chandrahmani254@gmail.com>
2025-08-31 16:15:13 +0530

    Add logo image to Navbar component

--- src/components/core/Navbar.tsx
+++ src/components/core/Navbar.tsx
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
+import Image from "next/image";
 
 const navLinks = [
   { text: "Home", href: "/" },
@@ -43,7 +44,7 @@ const Navbar = () => {
             fontWeight: "600",
           }}
         >
-          Hello Travel India
+         <Image src="/imageLogo.png" width={150} height={0} alt="logo"/>
         </Typography>
 
         {!isMobile && (
